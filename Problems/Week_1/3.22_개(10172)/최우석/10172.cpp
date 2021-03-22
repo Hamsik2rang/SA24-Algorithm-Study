5ee833ef9afb339c5fd761c543d901de171ceff2
CHOIUSEOK <useok7008@gmail.com>
2021-03-22 15:06:22 +0900

    [최우석/C++/성공] B10172 - 개

---
+++ Problems/Week_1/3.22_개(10172)/최우석/10172.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "|\\_/|" << std::endl;
+    cout << "|q p|   /}" << std::endl;
+    cout << "( 0 )\"\"\"\\" << std::endl;
+    cout << "|\"\^\"\`    |" << std::endl;
+    cout << "||_/=\\\\__|" << std::endl;
+
+    return 0;
+}
