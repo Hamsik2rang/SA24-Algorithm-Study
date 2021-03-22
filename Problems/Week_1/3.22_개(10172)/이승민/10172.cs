07787b0cefd09548b51d47447ebf91e6beb0cc3b
Lesin928 <seung1084@gmail.com>
2021-03-22 13:33:09 +0900

    [이승민/C#/성공] B10172 - 개

---
+++ Problems/Week_1/3.22_개(10172)/이승민/10172.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+          //개_1번_브론즈_
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+
+        }
+    }
+}
+
