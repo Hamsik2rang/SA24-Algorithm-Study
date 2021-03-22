f9ab2bc06f93c4240b88049a525599bbc9338695
babEbab <dasomchoi52@gmail.com>
2021-03-22 21:19:37 +0900

    [최다솜/Java/성공] B10172 - 개

---
+++ Problems/Week_1/3.22_개(10172)/최다솜/10172.java
@@ -0,0 +1,16 @@
+/* to solve BaekJoon */
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
