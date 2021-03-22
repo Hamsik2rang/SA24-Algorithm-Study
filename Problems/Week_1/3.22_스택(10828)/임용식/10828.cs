6304d56a4cc9226d2e1c2f28398bfbe933e957ec
Im Yongsik <lvhi7121@gmail.com>
2021-03-22 10:19:50 +0900

    [임용식/C#/성공] S10828 - 스택

---
+++ Problems/Week_1/3.22_스택(10828)/임용식/10828.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace AlgoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commandCount;
+            commandCount = Convert.ToInt32(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            int[] stack = new int[commandCount];
+            int ptr = 0;
+
+            while (commandCount-- > 0)
+            {
+                string[] temp = Console.ReadLine().Split();
+                string command = temp[0].ToLower();
+                int data = 0;
+                if (temp.Length > 1)
+                {
+                    data = Convert.ToInt32(temp[1]);
+                }
+                if (command == "push")
+                {
+                    stack[ptr++] = data;
+                }
+                else if (command == "pop")
+                {
+                    if (ptr > 0)
+                    {
+                        sb.Append(stack[--ptr] + "\n");
+                    }
+                    else
+                    {
+                        sb.Append(-1 + "\n");
+                    }
+                }
+                else if (command == "size")
+                {
+                    sb.Append(ptr + "\n");
+                }
+                else if (command == "empty")
+                {
+                    sb.Append((ptr != 0 ? 0 : 1) + "\n");
+                }
+                else if (command == "top")
+                {
+                    if (ptr == 0)
+                    {
+                        sb.Append(-1 + "\n");
+                    }
+                    else
+                    {
+                        sb.Append(stack[ptr - 1] + "\n");
+                    }
+                }
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
