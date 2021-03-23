eb93c3c4b2b4fc7a8c87ce6f01fcda1e777fb3a2
CHOIUSEOK <useok7008@gmail.com>
2021-03-23 11:50:36 +0900

    [최우석/C++/성공] B10430 - 나머지

---
+++ Problems/Week_1/3.23_나머지(10430)/최우석/10430.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	
+	cin >> a >> b >> c;
+	
+	cout << (a + b) % c << std::endl;
+	cout << ((a % c) + (b % c)) % c << std::endl;
+	cout << (a * b) % c << std::endl;
+	cout << ((a % c) * (b % c) % c) << std::endl;
+
+	return 0;
+}
