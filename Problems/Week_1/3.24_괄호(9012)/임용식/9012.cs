0359db10e4053590b84fbddc186239e152ca7cda
Im Yongsik <lvhi7121@gmail.com>
2021-03-24 09:37:19 +0900

    [임용식/C#/성공] S9012 - 괄호

---
+++ Problems/Week_1/3.24_괄호(9012)/임용식/9012.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoCSharp
+{
+    class P2885
+    {
+        static void Main(string[] args)
+        {
+            int t;
+            StringBuilder sb = new StringBuilder();
+            t = Convert.ToInt32(Console.ReadLine());
+
+            while (t-- > 0)
+            {
+                int stack = 0;
+                bool isValid = true;
+                string ps = Console.ReadLine();
+
+                foreach (char p in ps)
+                {
+                    if (p == '(')
+                    {
+                        stack++;
+                    }
+                    else if (stack > 0)
+                    {
+                        stack--;
+                    }
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (stack > 0)
+                {
+                    isValid = false;
+                }
+                sb.Append((isValid ? "YES\n" : "NO\n"));
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
