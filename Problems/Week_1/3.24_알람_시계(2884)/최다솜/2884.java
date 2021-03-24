fc53f6cbcd0d552554ca6fc14a2d774805bb46e9
babEbab <dasomchoi52@gmail.com>
2021-03-24 18:12:29 +0900

    [최다솜/Java/성공] B2884 - 알람시계

---
+++ Problems/Week_1/3.24_알람_시계(2884)/최다솜/2884.java
@@ -0,0 +1,34 @@
+/* to solve BaekJoon */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Bronze {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+
+        minute += hour * 60;
+        if (minute < 45) {
+            minute += 60 * 24;
+        }
+
+        minute = minute - 45;
+        hour = minute / 60;
+        minute = minute % 60;
+
+        System.out.println(hour + " " + minute);
+        System.out.println(sb);
+        br.close();
+    }
+}
