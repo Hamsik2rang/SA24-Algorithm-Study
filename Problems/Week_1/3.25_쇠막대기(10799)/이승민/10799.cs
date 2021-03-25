5367b9dfc6bde5c9f8630cd796d9b6e8ff63e491
Lesin928 <seung1084@gmail.com>
2021-03-25 22:54:14 +0900

    [이승민/C#/성공] S10799 - 쇠막대기

---
+++ Problems/Week_1/3.25_쇠막대기(10799)/이승민/10799.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int stick = 0;
+            int piece = 0;
+
+            string[] words = new string[word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                words[i] = word.Substring(i, 1);
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (words[i] == "(")
+                {
+                    if(words[i+1] == "(")
+                    stick++;
+                }
+                else if (words[i] == ")")
+                {
+                    if (words[i-1] == "(")
+                    {
+                        //레이저 라는 뜻
+                        if (stick > 0)
+                        {
+                            piece = piece + stick;
+                        }
+
+                    }
+                    else if (words[i-1] == ")")
+                    {
+                        stick--;
+                        piece++;
+                    }
+                }
+            }
+            Console.WriteLine(piece);
+        }
+    }
+}
+
