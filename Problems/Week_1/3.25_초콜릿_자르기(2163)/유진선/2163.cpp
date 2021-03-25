fa33ef9cd54105783f1d087f4e551b10d8409f86
MechuriS <wlstjs5515@naver.com>
2021-03-25 11:39:22 +0900

    [유진선/C/실패] B2163 - 초콜릿자르기

---
+++ Problems/Week_1/3.25_초콜릿_자르기(2163)/유진선/2163.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int N, M;
+    int anser;
+    printf("N과 M을 순서대로 입력하세요.\n");
+    scanf_s("%d %d", &N, &M);
+    anser = N * M - 1;
+    printf("%d", anser);
+
+    return 0;
+}
