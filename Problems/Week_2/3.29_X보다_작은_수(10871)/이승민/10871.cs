b7dbd070928ff7402a0256b6ec33678a8726bd13
Lesin928 <seung1084@gmail.com>
2021-03-29 22:06:29 +0900

    [이승민/C#/성공] B10871 - X보다 작은 수

---
+++ Problems/Week_2/3.29_X보다_작은_수(10871)/이승민/10871.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Text;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string []NX = Console.ReadLine().Split(' ');
+
+            int [] sequence = new int[Int32.Parse(NX[0])];
+            int k = Int32.Parse(NX[1]);
+            StringBuilder sb = new StringBuilder();
+
+            string[] se = Console.ReadLine().Split(' ');
+            
+            for (int i = 0; i < se.Length; i++)
+            {
+                sequence[i] = Int32.Parse(se[i]);
+                if (sequence[i] < k)
+                {
+                    sb.Append(sequence[i] + " ");
+                }
+            }
+
+            Console.WriteLine(sb);
+
+        }
+
+    }
+
+    
+}
+
+*/
