bfccdb67e650b5cb82b6fee289e02a39efc3f9e2
CHOIUSEOK <useok7008@gmail.com>
2021-03-30 20:36:08 +0900

    [최우석/C++/성공] B2439 - 별 찍기 - 2

---
+++ Problems/Week_2/3.30_별_찍기_2(2439)/최우석/2439.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a;
+	cin >> a;
+
+	for (int b = 0; b < a; b++)
+	{
+		for (int c = 0; c < a-b-1; c++)
+		{
+			cout << ' ';
+		}
+		
+		for (int d = 0; d < b + 1; d++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
