fc90b618c8b831b841482626515e39c64cc71ce8
Im Yongsik <lvhi7121@gmail.com>
2021-03-31 10:22:08 +0900

    [임용식/C/성공] B1977 - 완전제곱수

---
+++ Problems/Week_2/3.31_완전제곱수(1977)/임용식/1977.c
@@ -0,0 +1,37 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main()
+{
+	int m, n;
+	int min = 0, sum = 0;
+	int isMinFounded = FALSE;
+	scanf("%d %d", &m, &n);
+
+	for (int i = 1; i * i <= n; i++)
+	{
+		if (m <= i * i)
+		{
+			sum += i * i;
+			if (!isMinFounded)
+			{
+				isMinFounded = TRUE;
+				min = i * i;
+			}
+		}
+	}
+
+	if (!isMinFounded)
+	{
+		printf("%d", -1);
+	}
+	else
+	{
+		printf("%d\n%d", sum, min);
+	}
+
+	return 0;
+}
