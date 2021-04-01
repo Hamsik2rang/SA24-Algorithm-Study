f2344132709f90f8b19b63cb1b93cbd536149927
Im Yongsik <lvhi7121@gmail.com>
2021-04-01 10:15:28 +0900

    [임용식/C#/성공] S12789 - 도키도키 간식드리미

---
+++ Problems/Week_2/4.1_도키도키_간식드리미(12789)/임용식/12789.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q               = new Queue<int>();
+            var tempLine        = new Stack<int>();
+            int n               = Convert.ToInt32(Console.ReadLine());
+            int[] personCount   = Array.ConvertAll(Console.ReadLine().Split(), s => Convert.ToInt32(s));
+
+            foreach (var i in personCount)
+            {
+                q.Enqueue(i);
+            }
+
+            int num = 1;
+
+            while (num <= n)
+            {
+                if (q.Count == 0 && tempLine.Peek() != num)
+                {
+                    Console.Write("Sad");
+                    return;
+                }
+                if (q.Count != 0 && q.Peek() == num)
+                {
+                    q.Dequeue();
+                    num++;
+                }
+                else if (tempLine.Count != 0 && tempLine.Peek() == num)
+                {
+                    tempLine.Pop();
+                    num++;
+                }
+                else
+                {
+                    while (q.Count > 0 && q.Peek() != num)
+                    {
+                        tempLine.Push(q.Dequeue());
+                    }
+                }
+            }
+            Console.Write("Nice");
+        }
+    }
+}
