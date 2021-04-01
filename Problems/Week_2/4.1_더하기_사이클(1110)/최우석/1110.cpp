925221f92874ef4263d91561e7f836f5275a5ea9
CHOIUSEOK <useok7008@gmail.com>
2021-04-02 03:31:32 +0900

    [최우석/C++/성공] B1110 - 더하기 사이클

---
+++ Problems/Week_2/4.1_더하기_사이클(1110)/최우석/1110.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b, c, d, e, f;
+	int i = 1;
+	cin >> a;
+
+	b = a / 10;
+	c = a % 10;
+	d = b;
+	e = c;
+	while (1)
+	{
+		f = c;
+		c = (b + c) % 10;
+		b = f;
+		if (b == d && c == e)
+		{
+			cout << i;
+			break;
+		}
+		
+		i += 1;
+	}
+
+	return 0;
+}
