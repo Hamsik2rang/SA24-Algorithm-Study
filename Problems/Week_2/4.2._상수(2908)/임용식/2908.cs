b0e6e4bf8c40a8c079c21eced46108c019ba3d12
Im Yongsik <lvhi7121@gmail.com>
2021-04-02 10:51:01 +0900

    [임용식/C#/성공] B2908 - 상수

---
+++ Problems/Week_2/4.2._상수(2908)/임용식/2908.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlgoCSharp
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Array.ConvertAll(Console.ReadLine().Split(), (s)=>
+            {
+                var temp = s.ToCharArray();
+                Array.Reverse(temp);
+                return Convert.ToInt32(new string(temp));
+            });
+
+            Console.Write(num[0] < num[1] ? num[1] : num[0]);
+        }
+    }
+}
