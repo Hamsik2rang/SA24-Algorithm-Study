35b3a07126a92ca2337378224c7167465c936537
Lesin928 <seung1084@gmail.com>
2021-04-06 00:35:20 +0900

    [이승민/C#/성공] B2577 - 숫자의 개수

---
+++ Problems/Week_3/4.5_숫자의_개수(2577)/이승민/2577.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] ABC = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                ABC[i] = Int32.Parse(Console.ReadLine());
+            }
+            string A = Convert.ToString(ABC[0]* ABC[1]* ABC[2]);
+
+            string[] words = A.Split(new string[] { "0" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "1" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "2" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "3" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "4" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "5" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "6" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "7" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "8" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+            words = A.Split(new string[] { "9" }, StringSplitOptions.None);
+            Console.WriteLine(words.Length - 1);
+
+        }
+    }
+}
