1107ef0487c7fe2140179361461a246f58b04c06
Kim Hee Ah <khak1998@naver.com>
2021-04-05 20:01:53 +0900

    [김희아/C#/성공]B2577-숫자의 개수

---
+++ Problems/Week_3/4.5_숫자의_개수(2577)/김희아/숫자의_개수.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+
+//namespace _20210405
+//{
+//    class 숫자의_개수
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = Convert.ToInt32(Console.ReadLine());
+//            int b = Convert.ToInt32(Console.ReadLine());
+//            int c = Convert.ToInt32(Console.ReadLine());
+
+//            String result = ""+ (a * b * c);
+
+//            int[] answer = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+//            for(int i = 0; i < result.Length; i++)
+//            {
+//                answer[Convert.ToInt32(result.Substring(i,1))] += 1;
+//            }
+
+//            for (int i = 0; i < answer.Length; i++)
+//            {
+//                Console.WriteLine(answer[i]);
+//            }
+//        }
+//    }
+//}
