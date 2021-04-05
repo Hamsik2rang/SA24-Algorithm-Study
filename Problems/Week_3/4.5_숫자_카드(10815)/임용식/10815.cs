569fe3e0617c7e3d737cac95942a0c7f353f2d0b
Im Yongsik <lvhi7121@gmail.com>
2021-04-05 10:51:33 +0900

    [임용식/C#/성공] S10815 - 숫자 카드

---
+++ Problems/Week_3/4.5_숫자_카드(10815)/임용식/10815.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+
+namespace AlgoCSharp
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.ReadLine();
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+            Console.ReadLine();
+            int[] searchData = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+            Array.Sort(arr);
+
+            foreach (var i in searchData)
+            {
+                sb.Append((Array.BinarySearch(arr, i) < 0 ? 0 : 1) + " ");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
