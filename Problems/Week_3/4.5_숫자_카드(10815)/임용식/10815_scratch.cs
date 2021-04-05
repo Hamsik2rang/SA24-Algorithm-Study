7aea8965f071e131fb5685283192924456c9a606
Im Yongsik <lvhi7121@gmail.com>
2021-04-05 10:51:57 +0900

    [임용식/C#/성공] S10815 - 숫자 카드
    BinarySearch 직접 구현한 버전

---
+++ Problems/Week_3/4.5_숫자_카드(10815)/임용식/10815_scratch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+
+namespace AlgoCSharp
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.ReadLine();
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+            Console.ReadLine();
+            int[] searchData = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+            Array.Sort(arr);
+
+            foreach (var i in searchData)
+            {
+                sb.Append(BinarySearch(arr, 0, arr.Length - 1, i) + " ");
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        static int BinarySearch(int[] arr, int left, int right, int value)
+        {
+            if (left > right)
+            {
+                return 0;
+            }
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == value)
+            {
+                return 1;
+            }
+            else if (arr[mid] > value)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+
+            return BinarySearch(arr, left, right, value);
+        }
+    }
+}
