ce214bcfde2fbe22a22f99f33bece06f10213b0f
Lesin928 <seung1084@gmail.com>
2021-04-06 13:48:51 +0900

    [이승민/C#/성공] B8958 -OX퀴즈

---
+++ Problems/Week_3/4.6_OX퀴즈(8958)/이승민/8958.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Linq;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+
+            int N = Int32.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                int a = 0; //누적점수
+                int b = 1; //연속된 O의 수
+                char[] ox = Console.ReadLine().ToArray();
+                for (int k = 0; k < ox.Length; k++)
+                {
+                    if (ox[k] == 'O')
+                    {
+                        a += b;
+                        b++;
+                    }
+                    else
+                        b = 1;
+                }
+                sb.Append(a + "\n");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
