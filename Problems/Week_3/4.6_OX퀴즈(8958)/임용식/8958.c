23dc431f28fe2e67df71097249414ff251acb602
Im Yongsik <lvhi7121@gmail.com>
2021-04-06 09:19:12 +0900

    [임용식/C/성공] B8958 - OX퀴즈

---
+++ Problems/Week_3/4.6_OX퀴즈(8958)/임용식/8958.c
@@ -0,0 +1,26 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int t;
+
+	scanf("%d", &t);
+
+	while (t--)
+	{
+		char result[80];
+		scanf("%s", result);
+		int point = 0;
+		int score = 0;
+		for (size_t i = 0; i < strlen(result); i++)
+		{
+			result[i] == 'O' ? point++ : (point = 0);
+			score += point;
+		}
+		printf("%d\n", score);
+	}
+
+	return 0;
+}
