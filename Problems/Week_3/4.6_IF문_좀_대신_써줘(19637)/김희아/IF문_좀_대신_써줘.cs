7102ed55fe136d355413e125565972dbf784c785
Kim Hee Ah <khak1998@naver.com>
2021-04-06 17:07:56 +0900

    [김희아/C#/실패] S19637 -IF문_좀_대신_써줘
    이분탐색시러

---
+++ Problems/Week_3/4.6_IF문_좀_대신_써줘(19637)/김희아/IF문_좀_대신_써줘.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace _20210406
+{
+    class IF문_좀_대신_써줘
+    {
+        static void Main(String[] args)
+        {
+            String[] nm = Console.ReadLine().Split();
+            int n = Convert.ToInt32(nm[0]);
+            int m = Convert.ToInt32(nm[1]);
+
+            Dictionary<String, int> condition = new Dictionary<String, int>();
+
+            for(int i = 0; i < n; i++)
+            {
+                String[] stateValue = Console.ReadLine().Split();
+                String state = stateValue[0];
+                int value = Convert.ToInt32(stateValue[1]);
+                condition.Add(state, value);
+            }
+
+            List<int> strong=new List<int>();
+
+            for(int i = 0; i < m; i++)
+            {
+                strong.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+
+            for(int i = 0; i < strong.Count; i++)
+            {
+                Console.WriteLine(condition.FirstOrDefault(x=>x.Value>=strong[i]).Key);
+            }
+
+        }
+    }
+}
