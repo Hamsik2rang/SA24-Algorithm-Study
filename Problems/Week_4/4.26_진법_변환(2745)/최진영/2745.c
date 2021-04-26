9529d5807c39ee9694f82fa95bcd8ca06bb17e9d
SjaegerS <80696106+SjaegerS@users.noreply.github.com>
2021-04-27 00:52:33 +0900

    [최진영/C/조언받아 성공]B2745-진법 변환

--- Problems/Week_4/4.26_진법_변환(2745)/최진영/2745.c
+++ Problems/Week_4/4.26_진법_변환(2745)/최진영/2745.c
@@ -5,25 +5,24 @@ int main()
 {
 	int B = 0, antil = 0;
 	long long int hap=0;
-	char N[7] = { 0, };
+	char N[33] = { 0, };
 	scanf("%s %d", &N, &B);
-	for (int i = 0; i <=6; i++)
+	for (int i = 0; i<=32; i++)
 	{
-		if (N[6-i] != 0)
+		if (N[32-i] != 0)
 		{
-			if (N[6 - i] > 64 && N[6 - i] <= 90)
+			if (N[32 - i] > 64 && N[32 - i] <= 90)
 			{
-				N[6 - i] = N[6 - i] - 55;
+				N[32 - i] = N[32 - i] - 55;
 			}
-			else if (N[6 - i] >= 48 && N[6 - i] < 85)
+			else if (N[32 - i] >= 48 && N[32 - i] < 85)
 			{
-				N[6 - i] = N[6 - i] - 48;
+				N[32 - i] = N[32 - i] - 48;
 			}
-			hap += N[6 - i] * pow(B, antil);
+			hap += N[32 - i] * pow(B, antil);
 			antil++;
 		}
 	}
-	printf("%lld\n", hap);
+	printf("%lld", hap);
 	return 0;
 }
-//살려주세요. 스스로 테스트 20번 돌렸을땐 답이 나오는데 틀렸데요.
