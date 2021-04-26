d99a4fa2c61691372cc41d9f241d85ca7a2fc554
Im Yongsik <lvhi7121@gmail.com>
2021-04-26 10:55:28 +0900

    [임용식/C/성공] B2745 - 진법 변환

---
+++ Problems/Week_4/4.26_진법_변환(2745)/임용식/2745.c
@@ -0,0 +1,44 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+
+int IsNumber(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int CharToInteger(char c)
+{
+	if (IsNumber(c))
+	{
+		return c - '0';
+	}
+	return c - 'A' + 10;
+}
+
+int ConvertBase(char n[], int base)
+{
+	int result = 0;
+	int length = strlen(n);
+
+	for (int i = 0; i <length; i++)
+	{
+		result *= base;
+		result += CharToInteger(n[i]);
+	}
+
+	return result;
+}
+
+int main()
+{
+	char n[33];
+	int base = 0;
+
+	scanf("%s", n);
+	scanf("%d", &base);
+
+	printf("%d", ConvertBase(n, base));
+
+	return 0;
+}
