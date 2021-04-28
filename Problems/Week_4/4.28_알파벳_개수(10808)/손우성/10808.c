1e09cd5b0a98ddeb6a9e02cea1b60269709c1319
dntjd <jjok1021@naver.com>
2021-04-29 01:02:12 +0900

    [손우성/C/성공]B10808 알파벳 개수

---
+++ Problems/Week_4/4.28_알파벳_개수(10808)/손우성/10808.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int result(char alphabet);
+
+int result(char alphabet) {
+	int count[26] = { 0, };
+	if (alphabet >= 97 && alphabet <= 122) {
+		count[(int)alphabet - 97] += 1;
+	}
+	return count[(int)alphabet - 97];
+}
+
+int main() {
+	int num[26] = { 0, };
+	char word[100] = { 0, };
+	scanf("%s", word);
+	for (int i = 0;; i++) {
+		if (word[i] == 0) {
+			break;
+		}
+		num[(int)word[i] - 97] += result(word[i]);
+	}
+	for (int j = 0; j <= 25; j++) {
+		printf("%d ", num[j]);
+	}
+	return 0;
+}
