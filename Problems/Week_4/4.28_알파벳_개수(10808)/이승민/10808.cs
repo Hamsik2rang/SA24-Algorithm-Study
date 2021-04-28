5593a803eeca7d35efc6911bff68f4382a0b7be4
Lesin928 <seung1084@gmail.com>
2021-04-28 23:29:59 +0900

    [이승민/C#/성공] B10808 -알파벳 갯수

---
+++ Problems/Week_4/4.28_알파벳_개수(10808)/이승민/10808.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+
+namespace 백준1
+
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] Sword = str.ToArray();
+            int[] alpa = new int[26];  //a 97~122 z
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < Sword.Length; i++)
+            {
+                alpa[(int)Sword[i] - 97]++;
+            }
+
+            for (int i = 0; i < alpa.Length; i++)
+            {
+                sb.Append(alpa[i]);
+                sb.Append(" ");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
+
