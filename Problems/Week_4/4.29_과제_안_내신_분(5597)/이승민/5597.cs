5a38a44184010e037a53444f7dceb0f5b69aab4b
Lesin928 <seung1084@gmail.com>
2021-04-30 00:05:48 +0900

    [이승민/C#/성공] B5597-과제 안 내신 분

---
+++ Problems/Week_4/4.29_과제_안_내신_분(5597)/이승민/5597.cs
@@ -0,0 +1,27 @@
+﻿/*using System;
+using System.Text;
+using System.Linq;
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] student = new int[30];
+
+            for (int i = 0; i < 28; i++)
+            {
+                student[Convert.ToInt32(Console.ReadLine()) - 1] += 1;
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                if (student[i] != 1)
+                {
+                    Console.WriteLine(i + 1);
+                } 
+            }
+
+        }
+    }
+}*/
