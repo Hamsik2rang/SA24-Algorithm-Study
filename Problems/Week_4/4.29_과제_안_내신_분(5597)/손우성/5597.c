d417c6d6076593c848ccc11a95659724666d9f26
dntjd <jjok1021@naver.com>
2021-05-01 00:07:54 +0900

    [손우성/C/성공]B5597 - 과제 안 내신 분..?

---
+++ Problems/Week_4/4.29_과제_안_내신_분(5597)/손우성/5597.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+	int student[31] = { 0, };
+	int reporter;
+
+	for (int i = 1; i < 29; i++){
+		scanf("%d", &reporter);
+		student[reporter] += 1;
+	}
+	for (int j = 1; j <= 30; j++){
+		if (student[j] == 0){
+			printf("%d\n", j);
+			}
+	}
+	return 0;
+}
