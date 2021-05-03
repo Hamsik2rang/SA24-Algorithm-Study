b8d537855434d7b8226d49400b99733b670f8d95
Lesin928 <seung1084@gmail.com>
2021-05-03 20:37:48 +0900

    [이승민/C#/성공] B10872 - 팩토리얼

---
+++ Problems/Week_5/5.3_팩토리얼(10872)/이승민/10872.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            for (int M = 0; M < N; M++)
+            {
+                sum *= N - M;
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+
+}
