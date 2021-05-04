c036b31834f66e04fe3078096c43be05892d2e8e
Lesin928 <seung1084@gmail.com>
2021-05-04 22:53:33 +0900

    [이승민/C#/성공] B10870 - 피보나치수5

---
+++ Problems/Week_5/5.4_피보나치_수_5(10870)/이승민/10870.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int n2 = 0;
+            int n1 = 1;
+            int n = 0;
+            for (int i = 0; i < N; i++)
+            {
+                n = n2 + n1;
+                n2 = n1;
+                n1 = n;
+            }
+            Console.WriteLine(n2);
+
+        }
+
+    }
+    
+
+}
