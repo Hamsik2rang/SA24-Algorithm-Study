c8120b6bc519dda29c77a671b4068d316c00db24
sbten10 <sbten10@icloud.com>
2021-05-10 15:15:22 +0900

    [이세빈/C/성공] S7568 - 덩치

--- Problems/Week_6/5.10_덩치(7568)/이세빈/7568.c
+++ Problems/Week_6/5.10_덩치(7568)/이세빈/7568.c
@@ -1,26 +1,28 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	int N, k = 0;
-	int x[50] = { 0, };
-	int y[50] = { 0, };
+    int i, j, input_num, count = 0;
+    int x[51] = { 0 };
+    int y[51] = { 0 };
 
-	scanf("%d", &N);
+    scanf("%d", &input_num);
 
-	for (int i = 0; i < N; i++)
-		scanf("%d %d", &x[i], &y[i]);
+    for (i = 0; i < input_num; i++)
+    {
+        scanf("%d %d", &x[i], &y[i]);
+    }
+    for (i = 0; i < input_num; i++)
+    {
+        count = 1;
 
-	for (int i = 0; i < N; i++)
-	{
-		k = 0;
-		for (int j = 0; j < N; j++)
-		{
-			if (x[i] < x[j] && y[i] < y[j])
-				k++;
-		}
-		printf("%d", k + 1);
-	}
-	return 0;
+        for (j = 0; j < input_num; j++)
+        {
+            if (x[i] < x[j] && y[i] < y[j])
+
+                count++;
+        }
+        printf("%d ", count);
+    }
 }
