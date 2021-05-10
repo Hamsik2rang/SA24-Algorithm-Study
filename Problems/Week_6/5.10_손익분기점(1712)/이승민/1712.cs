ba571336e951a0435fc09a690f5447523033e54e
Lesin928 <seung1084@gmail.com>
2021-05-10 22:11:18 +0900

    [이승민/C#/실패] B1712 - 손익분기점
    (런타임 에러 (NZEC))

---
+++ Problems/Week_6/5.10_손익분기점(1712)/이승민/1712.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = Console.ReadLine().Split(' ');
+            int [] ABC = new int[3];
+            for (int i = 0; i < ABC.Length; i++)
+            {
+                ABC[i] = int.Parse(abc[i]);
+            }
+
+
+            if((ABC[2] - ABC[1]) < 0 )
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                int a = ABC[0] / (ABC[2] - ABC[1]) + 1;
+                Console.WriteLine(a);
+            }
+
+        }
+    }
+}
+
