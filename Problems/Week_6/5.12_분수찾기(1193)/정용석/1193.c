be7a388551cfd1034febdeb9e2ae4e9a1ffcde8c
strawberry-pie <80702782+strawberry-pie@users.noreply.github.com>
2021-05-12 20:01:58 +0900

    [정용석/C/실패] B1193 - 분수찾기
    이해가 어려워 다른 분의 것을 보고 이해하였습니다.

---
+++ Problems/Week_6/5.12_분수찾기(1193)/정용석/1193.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b = 1;
+	int i;
+
+	scanf("%d", &a);
+
+	for (i = 1; a > b; )
+		b += ++i;
+
+	if (i % 2 == 0)
+		printf("%d/%d", i - (b - a), 1 + (b - a));
+
+	else
+		printf("%d/%d", 1 + (b - a), i - (b - a));
+
+	return 0;
+}
