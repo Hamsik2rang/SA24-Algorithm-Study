1499781a636537f0b9c40f660511b933d2fdfa9b
Im Yongsik <lvhi7121@gmail.com>
2021-05-19 23:17:25 +0900

    [임용식/C#/성공] B13706 - 제곱근

---
+++ Problems/Week_7/5.19_제곱근(13706)/임용식/13706.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace AlgoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bint = BigInteger.Parse(Console.ReadLine());
+
+            Solution(1, bint);
+        }
+
+        static void Solution(BigInteger left, BigInteger right)
+        {
+            var target = right;
+            while (left <= right)
+            {
+                BigInteger mid = (left+right)/2;
+                var comp = BigInteger.Pow(mid,2);
+                if (comp == target)
+                {
+                    Console.WriteLine(mid);
+                    break;
+                }
+                else if (comp < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+    }
+}
