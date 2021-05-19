322857dcad986bcfd2bfee3c7106a7b479b3dbff
Lesin928 <seung1084@gmail.com>
2021-05-19 17:20:42 +0900

    [이승민/C#/실패] B13706 - 제곱근
    반례를 못찾겠음

---
+++ Problems/Week_7/5.19_제곱근(13706)/이승민/13706.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Double n = Convert.ToDouble(Console.ReadLine());
+            Double low = 1;
+            Double high = n;
+            Double mid;
+            while(true)
+            {          
+                mid = Math.Truncate((low + high) / 2);
+                if (mid * mid == n)
+                {
+                    Console.WriteLine(mid);
+                    break;
+                }
+                else if (mid * mid > n)
+                {
+                    high = mid - 1.0;
+                }
+                else if (mid * mid < n)
+                {
+                    low = mid + 1.0;
+                }
+            }
+            
+
+        }
+    }
+}
