f9da0225995af47ffb82dd5ae9bd013903aff74b
Lesin928 <seung1084@gmail.com>
2021-05-24 20:48:05 +0900

    [이승민/C#/실패] - B17266
    런타임오류

---
+++ Problems/Week_8/5.24_어두운_굴다리(17266)/이승민/17266.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+namespace 백준1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            string [] y = Console.ReadLine().Split(' ');
+            int[] x = new int[M];
+            for (int i = 0; i < y.Length; i++)
+            {
+                x[i] = Convert.ToInt32(y[i]); 
+            }
+            int H = x[0];
+
+            if (N - x[M - 1] > H)
+            {
+                H = N - x[M - 1];
+            }
+
+            for (int i = 1; i < M; i++)
+            {
+                if ((x[i] - x[i - 1]) > H)
+                {
+                    H = x[i + 1] - (x[i]);
+                }
+            }
+
+            Console.WriteLine(H);
+
+        }
+    }
+}
+
